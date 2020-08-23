e85509fdae6d1b1a0d47d4fcc293c7c038e8783e
Nathan Bailey <dbailey@k12k.com>
2020-08-23 13:04:50 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,8 +3,29 @@ info.set_score(0)
 info.set_life(3)
 
 # Setup the Player
+spaceship = sprites.create(img("""
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . 2 2 . . . . . . . . . . . .
+    . . 2 2 2 2 . . . . . . . . . .
+    . . 2 2 2 2 2 . . . . . . . . .
+    . . 4 4 4 4 4 2 . . . . . . . .
+    . . 4 4 4 4 4 2 2 2 . . . . . .
+    . . 4 4 4 4 4 2 2 2 2 . . . . .
+    . . 4 4 4 4 4 2 2 2 . . . . . .
+    . . 4 4 4 4 4 2 . . . . . . . .
+    . . 2 2 2 2 2 . . . . . . . . .
+    . . 2 2 2 2 . . . . . . . . . .
+    . . 2 2 . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+"""))
+spaceship.set_position(10, scene.screen_height()/2)
+spaceship.set_flag(SpriteFlag.StayInScreen, True)
 
 # Configure Player Controls
+controller.move_sprite(spaceship, 200, 200)
 
 # Generate Enemies
 
--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
 //  Setup the Game
 info.setScore(0)
 info.setLife(3)
+//  Setup the Player
+let spaceship = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . 2 2 . . . . . . . . . . . .
+    . . 2 2 2 2 . . . . . . . . . .
+    . . 2 2 2 2 2 . . . . . . . . .
+    . . 4 4 4 4 4 2 . . . . . . . .
+    . . 4 4 4 4 4 2 2 2 . . . . . .
+    . . 4 4 4 4 4 2 2 2 2 . . . . .
+    . . 4 4 4 4 4 2 2 2 . . . . . .
+    . . 4 4 4 4 4 2 . . . . . . . .
+    . . 2 2 2 2 2 . . . . . . . . .
+    . . 2 2 2 2 . . . . . . . . . .
+    . . 2 2 . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`)
+spaceship.setPosition(10, scene.screenHeight() / 2)
+spaceship.setFlag(SpriteFlag.StayInScreen, true)
+//  Configure Player Controls
+controller.moveSprite(spaceship, 200, 200)
