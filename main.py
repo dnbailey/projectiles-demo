44ea063d3eb60935313323f55dc6e3f7dc4a01e9
Nathan Bailey <dbailey@k12k.com>
2020-08-23 12:54:39 -0400

    Update pxt.json, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Setup the Game
+info.set_score(0)
+info.set_life(3)
+
+# Setup the Player
+
+# Configure Player Controls
+
+# Generate Enemies
+
+# Shoot Enemies with Projectiles
--- main.ts
+++ main.ts
@@ -1,1 +1,3 @@
-
+//  Setup the Game
+info.setScore(0)
+info.setLife(3)
