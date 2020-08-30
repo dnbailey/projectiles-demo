b8e9f091e7c595249666407c313f0ec7d01ffacf
Nathan Bailey <dbailey@k12k.com>
2020-08-30 15:18:43 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,96 +1,105 @@
-# Setup the Game
-info.set_score(0)
-info.set_life(3)
-
-# Setup the Player
-spaceship = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . 2 2 . . . . . . . . . . . .
-    . . 2 2 2 2 . . . . . . . . . .
-    . . 2 2 2 2 2 . . . . . . . . .
-    . . 4 4 4 4 4 2 . . . . . . . .
-    . . 4 4 4 4 4 2 2 2 . . . . . .
-    . . 4 4 4 4 4 2 2 2 2 . . . . .
-    . . 4 4 4 4 4 2 2 2 . . . . . .
-    . . 4 4 4 4 4 2 . . . . . . . .
-    . . 2 2 2 2 2 . . . . . . . . .
-    . . 2 2 2 2 . . . . . . . . . .
-    . . 2 2 . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-"""))
-spaceship.set_position(10, scene.screen_height()/2)
-spaceship.set_flag(SpriteFlag.StayInScreen, True)
-spaceship.set_kind(SpriteKind.player)
-
-# Configure Player Controls
-controller.move_sprite(spaceship, 200, 200)
-
-# Generate Enemies
-def on_update_interval():
-    rock = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . c c c . . . . . . .
-    . . . . . c c b . c c . . . . .
-    . . . . . c c c c . c c c . . .
-    . . . . c c c c c c . c . . . .
-    . . . . c . c c c c c b . . . .
-    . . . c c c c c . . c c . . . .
-    . . . . . c b b c c b c . . . .
-    . . . . . c . c b c c . . . . .
-    . . . . . c c . c c c . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    """))
-    rock.set_position(scene.screen_width(), randint(0,scene.screen_height()))
-    rock.set_velocity(-50, 0)
-    rock.set_kind(SpriteKind.enemy)
-
-game.on_update_interval(750, on_update_interval)
-
 # Shoot Enemies with Projectiles
-def on_button_event_a_pressed():
+
+def on_player1_button_a_pressed():
+    global blast
     blast = sprites.create_projectile_from_sprite(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . 2 2 4 5 . . . . . .
-    . . . . . . 2 2 4 5 . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    """), spaceship, 50, 0)
-
-controller.player1.on_button_event(ControllerButton.A, ControllerButtonEvent.PRESSED, on_button_event_a_pressed)
-
-# Lose Life when Hit
-def on_overlap(sprite, otherSprite):
-    otherSprite.destroy()
-    info.change_life_by(-1)
-
-sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_overlap)
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . 2 2 4 5 . . . . . . 
+                    . . . . . . 2 2 4 5 . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        spaceship,
+        50,
+        0)
+controller.player1.on_button_event(ControllerButton.A,
+    ControllerButtonEvent.PRESSED,
+    on_player1_button_a_pressed)
 
 # Destroy Rock when Blasted
-def on_rock_blasted(sprite, otherSprite):
+
+def on_on_overlap(sprite, otherSprite):
     sprite.destroy()
     otherSprite.destroy(effects.fire, 100)
     info.change_score_by(1)
-    if info.score() % 10 is 0:
+    if info.score() % 10 == 0:
         if info.life() < 5:
             info.change_life_by(1)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_on_overlap)
 
-sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_rock_blasted)
+# Lose Life when Hit
+
+def on_on_overlap2(sprite, otherSprite):
+    otherSprite.destroy()
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
+rock: Sprite = None
+blast: Sprite = None
+spaceship: Sprite = None
+# Setup the Game
+info.set_score(0)
+info.set_life(3)
+# Setup the Player
+spaceship = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . 2 2 . . . . . . . . . . . . 
+            . . 2 2 2 2 . . . . . . . . . . 
+            . . 2 2 2 2 2 . . . . . . . . . 
+            . . 4 4 4 4 4 2 . . . . . . . . 
+            . . 4 4 4 4 4 2 2 2 . . . . . . 
+            . . 4 4 4 4 4 2 2 2 2 . . . . . 
+            . . 4 4 4 4 4 2 2 2 . . . . . . 
+            . . 4 4 4 4 4 2 . . . . . . . . 
+            . . 2 2 2 2 2 . . . . . . . . . 
+            . . 2 2 2 2 . . . . . . . . . . 
+            . . 2 2 . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    0)
+spaceship.set_position(10, scene.screen_height() / 2)
+spaceship.set_flag(SpriteFlag.STAY_IN_SCREEN, True)
+spaceship.set_kind(SpriteKind.player)
+# Configure Player Controls
+controller.move_sprite(spaceship, 200, 200)
+# Generate Enemies
+
+def on_update_interval():
+    global rock
+    rock = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . c c c . . . . . . . 
+                    . . . . . c c b . c c . . . . . 
+                    . . . . . c c c c . c c c . . . 
+                    . . . . c c c c c c . c . . . . 
+                    . . . . c . c c c c c b . . . . 
+                    . . . c c c c c . . c c . . . . 
+                    . . . . . c b b c c b c . . . . 
+                    . . . . . c . c b c c . . . . . 
+                    . . . . . c c . c c c . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        0)
+    rock.set_position(scene.screen_width(), randint(0, scene.screen_height()))
+    rock.set_velocity(-50, 0)
+    rock.set_kind(SpriteKind.enemy)
+game.on_update_interval(750, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,25 +1,68 @@
+//  Shoot Enemies with Projectiles
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function on_player1_button_a_pressed() {
+    
+    blast = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . 2 2 4 5 . . . . . . 
+                    . . . . . . 2 2 4 5 . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        `, spaceship, 50, 0)
+})
+//  Destroy Rock when Blasted
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    sprite.destroy()
+    otherSprite.destroy(effects.fire, 100)
+    info.changeScoreBy(1)
+    if (info.score() % 10 == 0) {
+        if (info.life() < 5) {
+            info.changeLifeBy(1)
+        }
+        
+    }
+    
+})
+//  Lose Life when Hit
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_on_overlap2(sprite: Sprite, otherSprite: Sprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
+let rock : Sprite = null
+let blast : Sprite = null
+let spaceship : Sprite = null
 //  Setup the Game
 info.setScore(0)
 info.setLife(3)
 //  Setup the Player
-let spaceship = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . 2 2 . . . . . . . . . . . .
-    . . 2 2 2 2 . . . . . . . . . .
-    . . 2 2 2 2 2 . . . . . . . . .
-    . . 4 4 4 4 4 2 . . . . . . . .
-    . . 4 4 4 4 4 2 2 2 . . . . . .
-    . . 4 4 4 4 4 2 2 2 2 . . . . .
-    . . 4 4 4 4 4 2 2 2 . . . . . .
-    . . 4 4 4 4 4 2 . . . . . . . .
-    . . 2 2 2 2 2 . . . . . . . . .
-    . . 2 2 2 2 . . . . . . . . . .
-    . . 2 2 . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`)
+spaceship = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . 2 2 . . . . . . . . . . . . 
+            . . 2 2 2 2 . . . . . . . . . . 
+            . . 2 2 2 2 2 . . . . . . . . . 
+            . . 4 4 4 4 4 2 . . . . . . . . 
+            . . 4 4 4 4 4 2 2 2 . . . . . . 
+            . . 4 4 4 4 4 2 2 2 2 . . . . . 
+            . . 4 4 4 4 4 2 2 2 . . . . . . 
+            . . 4 4 4 4 4 2 . . . . . . . . 
+            . . 2 2 2 2 2 . . . . . . . . . 
+            . . 2 2 2 2 . . . . . . . . . . 
+            . . 2 2 . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    `, 0)
 spaceship.setPosition(10, scene.screenHeight() / 2)
 spaceship.setFlag(SpriteFlag.StayInScreen, true)
 spaceship.setKind(SpriteKind.Player)
@@ -27,64 +70,26 @@ spaceship.setKind(SpriteKind.Player)
 controller.moveSprite(spaceship, 200, 200)
 //  Generate Enemies
 game.onUpdateInterval(750, function on_update_interval() {
-    let rock = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . c c c . . . . . . .
-    . . . . . c c b . c c . . . . .
-    . . . . . c c c c . c c c . . .
-    . . . . c c c c c c . c . . . .
-    . . . . c . c c c c c b . . . .
-    . . . c c c c c . . c c . . . .
-    . . . . . c b b c c b c . . . .
-    . . . . . c . c b c c . . . . .
-    . . . . . c c . c c c . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    `)
+    
+    rock = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . c c c . . . . . . . 
+                    . . . . . c c b . c c . . . . . 
+                    . . . . . c c c c . c c c . . . 
+                    . . . . c c c c c c . c . . . . 
+                    . . . . c . c c c c c b . . . . 
+                    . . . c c c c c . . c c . . . . 
+                    . . . . . c b b c c b c . . . . 
+                    . . . . . c . c b c c . . . . . 
+                    . . . . . c c . c c c . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        `, 0)
     rock.setPosition(scene.screenWidth(), randint(0, scene.screenHeight()))
     rock.setVelocity(-50, 0)
     rock.setKind(SpriteKind.Enemy)
-})
-//  Shoot Enemies with Projectiles
-controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function on_button_event_a_pressed() {
-    let blast = sprites.createProjectileFromSprite(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . 2 2 4 5 . . . . . .
-    . . . . . . 2 2 4 5 . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    `, spaceship, 50, 0)
-})
-//  Lose Life when Hit
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_overlap(sprite: Sprite, otherSprite: Sprite) {
-    otherSprite.destroy()
-    info.changeLifeBy(-1)
-})
-//  Destroy Rock when Blasted
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_rock_blasted(sprite: Sprite, otherSprite: Sprite) {
-    sprite.destroy()
-    otherSprite.destroy(effects.fire, 100)
-    info.changeScoreBy(1)
-    if (info.score() % 10 === 0) {
-        if (info.life() < 5) {
-            info.changeLifeBy(1)
-        }
-        
-    }
-    
 })
